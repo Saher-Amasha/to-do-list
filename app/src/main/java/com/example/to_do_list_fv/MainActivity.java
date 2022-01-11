b9c142432a1c6cb6510f5a2244649b9f5507ad7a
Saher-Amasha <saheramasha0@gmail.com>
2022-01-11 22:58:34 +0200

    initialized project.

---
+++ app/src/main/java/com/example/to_do_list_fv/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.to_do_list_fv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
