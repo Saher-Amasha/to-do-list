667f0f67f5219429f305a126a5909f6aceeca5ce
Saher-Amasha <saheramasha0@gmail.com>
2022-02-02 02:51:09 +0200

    added_login_and_signup_functionality_and_loginAndSignup_using_google

--- app/src/main/java/com/example/to_do_list_fv/LoginActivity.java
+++ app/src/main/java/com/example/to_do_list_fv/LoginActivity.java
@@ -2,23 +2,180 @@ package com.example.to_do_list_fv;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
 
 public class LoginActivity extends Activity {
+
+    private GoogleSignInClient mGoogleSignInClient;
+    private final static  int RC_SIGN_IN=123;
+    private FirebaseAuth mAuth;
+    TextInputLayout T_password,T_email;
+    TextView ET_email;
+
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_login);
-        Button goToSignUp = (Button) findViewById(R.id.go_to_signup);
-        goToSignUp.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
-                startActivity(intent);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        createRequest();
+
+
+        Button goToSignUp =  findViewById(R.id.go_to_signup);
+        goToSignUp.setOnClickListener(v -> {
+            Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+            startActivity(intent);
+        });
+
+        T_email=findViewById(R.id.login_email);
+        T_password=findViewById(R.id.login_password);
+        ET_email= findViewById(R.id.isEmailValidLogin);
+
+        Button loginUpButton =  findViewById(R.id.login_button);
+        loginUpButton.setOnClickListener(v -> logIN());
+        Objects.requireNonNull(T_email.getEditText()).addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void afterTextChanged(Editable s) { }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                    if (Objects.requireNonNull(T_email.getEditText()).getText().toString().length() != 0) {
+                        if (isEmailValid(T_email.getEditText().getText().toString())) {
+                            ET_email.setText(R.string.email_valid);
+                            ET_email.setTextColor(Color.GREEN);
+                        } else {
+                            ET_email.setText(R.string.email_not_valid);
+                            ET_email.setTextColor(Color.RED);
+                        }
+                }
+                else{
+                    ET_email.setText("");
+
+                }
             }
         });
     }
 
+    private void logIN() {
+        String email= Objects.requireNonNull(T_email.getEditText()).getText().toString();
+        String password= Objects.requireNonNull(T_password.getEditText()).getText().toString();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent intent = new Intent(LoginActivity.this, ProfileActivity.class);
+                            startActivity(intent);
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser account) {
+    }
+
+    private void createRequest() {
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+               //
 
 
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+            findViewById(R.id.sign_in_button).setOnClickListener(v -> signIn());
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                          FirebaseUser user = mAuth.getCurrentUser();
+                          Intent intent = new Intent(LoginActivity.this, ProfileActivity.class);
+                          startActivity(intent);
+                          updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                          updateUI(null);
+                        }
+                    }
+                });
+    }
+        public static boolean isEmailValid(String email)
+        {
+            String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                    "[a-zA-Z0-9_+&*-]+)*@" +
+                    "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                    "A-Z]{2,7}$";
+
+            Pattern pat = Pattern.compile(emailRegex);
+            if (email == null)
+                return false;
+            return pat.matcher(email).matches();
+        }
 }
